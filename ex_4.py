deb64393996c479ba5e5f7d7986b5450f427cd92
YaelSim <58948260+YaelSim@users.noreply.github.com>
2020-12-30 15:23:38 +0200

    done

--- ex_4.py
+++ ex_4.py
@@ -7,23 +7,16 @@ import torchvision as torchvision
 from torch.utils.data import SubsetRandomSampler
 
 
+# NOTE: All code parts that are commented out were used in order to test the different models
+#  according to the Pytorch FashionMNIST datasets.
 def main():
-    learning_list = [0.01, 0.001, 0.0005, 0.0009, 0.0015]
-    test_predictions = []
     batch = 64
     valid_ratio = 0.2
     epoches = 10
-    """
-    # option 1 - like ex3.
-    train_x, train_y, test_x = sys.argv[1], sys.argv[2], sys.argv[3]
-    train_x = np.loadtxt(train_x)
-    train_y = np.loadtxt(train_y)
-    test_x = np.loadtxt(test_x)
-    # Convert to tensors
-    train_x_tensor = torch.from_numpy(train_x)
-    train_y_tensor = torch.from_numpy(train_y)
-    test_x_tensor = torch.from_numpy(test_x)
-    """
+
+    # get the test from files and convert to Tensor
+    test_x = sys.argv[3]
+    test_x = np.loadtxt(test_x) / 255
 
     transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
                                                  torchvision.transforms.Normalize((0.1307,), (0.3081,))])
@@ -31,10 +24,14 @@ def main():
                                                   train=True,
                                                   download=True,
                                                   transform=transforms)
+    # we did it to check the best model
+    """
     test_set = torchvision.datasets.FashionMNIST(root='./data',
                                                  train=False,
                                                  download=True,
                                                  transform=transforms)
+    """
+    test_x_loader = transforms(test_x)[0].float()
 
     # Splits train data set to the corresponding validation ratio.
     train_set_size = len(train_set)
@@ -52,11 +49,15 @@ def main():
     validation_loader = torch.utils.data.DataLoader(train_set,
                                                     batch_size=batch,
                                                     sampler=validation_sampler)
+    # we did it to check the best model
+    """
     test_loader = torch.utils.data.DataLoader(test_set,
                                               batch_size=batch,
                                               shuffle=False)
+    """
 
-    learning_rate = 0.01  # The best learning rate found for model A
+    """
+    learning_rate = 0.1  # The best learning rate found for model A
     modelA = FirstNetwork(image_size=28 * 28)
     optimizer = torch.optim.SGD(modelA.parameters(), lr=learning_rate)
 
@@ -64,18 +65,19 @@ def main():
         print("model A, curr epoch: " + str(i))
         train(i, modelA, train_loader, learning_rate, optimizer)
         validate(modelA, validation_loader)
-    test_predictions = test(modelA, test_loader)
+    #test_predictions = test(modelA, test_loader)
+    """
 
     learning_rate = 0.001   # The best learning rate for Model B
     modelB = FirstNetwork(image_size=28 * 28)
     optimizer = torch.optim.Adam(modelB.parameters(), lr=learning_rate)
 
     for i in range(epoches):
-        print("model B, curr epoch: " + str(i))
         train(i, modelB, train_loader, learning_rate, optimizer)
         validate(modelB, validation_loader)
-    test_predictions = test(modelB, test_loader)
+    #test_predictions = test(modelB, test_loader)
 
+    """
     # The third model is just like modelA, but we drop out some of the neurons.
     learning_rate = 0.01  # The best learning rate for models C, D
     modelC = FirstNetworkWithDropout(image_size=28 * 28)
@@ -85,7 +87,7 @@ def main():
         print("model C, curr epoch: " + str(i))
         train(i, modelC, train_loader, learning_rate, optimizer)
         validate(modelC, validation_loader)
-    test_predictions = test(modelC, test_loader)
+    #test_predictions = test(modelC, test_loader)
 
     # The fourth model is just like modelA, but we add Batch Normalization
     modelD = FirstNetworkWithNormalization(image_size=28 * 28)
@@ -95,7 +97,7 @@ def main():
         print("model D, curr epoch: " + str(i))
         train(i, modelD, train_loader, learning_rate, optimizer)
         validate(modelD, validation_loader)
-    test_predictions = test(modelD, test_loader)
+    #test_predictions = test(modelD, test_loader)
 
     learning_rate = 0.0009  # The best learning rate for model E
     modelE = SecondNetwork(image_size=28 * 28)
@@ -106,7 +108,7 @@ def main():
         print("model E, curr epoch: " + str(i))
         train(i, modelE, train_loader, learning_rate, optimizer)
         validate(modelE, validation_loader)
-    test_predictions = test(modelE, test_loader)
+    #test_predictions = test(modelE, test_loader)
 
     learning_rate = 0.0005  # The best learning rate for model F
     modelF = ThirdNetwork(image_size=28 * 28)
@@ -116,8 +118,14 @@ def main():
         print("model F, curr epoch: " + str(i))
         train(i, modelF, train_loader, learning_rate, optimizer)
         validate(modelF, validation_loader)
-    test_predictions = test(modelF, test_loader)
-    # todo choose the best model and print its output to test_y!
+    #test_predictions = test(modelF, test_loader)
+    """
+
+    # after we checked all the models, we got that model B has the best accuracy
+    # so we get test_y from running test_x with model B.
+    prediction = predict_y(modelB, test_x_loader)
+    write_to_file(prediction)
+
 
 
 def train(epoch, model, train_loader, learning_rate, optimizer):
@@ -131,7 +139,6 @@ def train(epoch, model, train_loader, learning_rate, optimizer):
 
 
 def validate(model, validation_loader):
-    batch = 64
     model.eval()
     validation_loss = 0
     accuracy_counter = 0
@@ -139,18 +146,20 @@ def validate(model, validation_loader):
         for data, target in validation_loader:
             output = model(data)  # forward.
             # sum up batch loss and get the index of the max log-probability.
-            # test_loss += nn.nll_loss(output, target, size_average=False).item()
             validation_loss += nn.nll_loss(output, target, reduction='sum').item()
             prediction = output.max(1, keepdim=True)[1]
             accuracy_counter += prediction.eq(target.view_as(prediction)).cpu().sum()
 
+    """
     # Get the average loss
     validation_loss /= (len(validation_loader) * batch)
     print('\nValidation Set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         validation_loss, accuracy_counter, (len(validation_loader) * batch),
         100. * accuracy_counter / (len(validation_loader) * batch)))
+    """
 
 
+# we run this on every model and got the accuracy on each model
 def test(model, test_loader):
     prediction_list = []
     model.eval()
@@ -160,20 +169,32 @@ def test(model, test_loader):
         for data, target in test_loader:
             output = model(data)  # forward.
             # sum up batch loss and get the index of the max log-probability.
-            # test_loss += nn.nll_loss(output, target, size_average=False).item()
             test_loss += nn.nll_loss(output, target, reduction='sum').item()
             prediction = output.max(1, keepdim=True)[1]
             prediction_list.append(prediction)
             accuracy_counter += prediction.eq(target.view_as(prediction)).cpu().sum()
 
+    """
     # Get the average loss
     test_loss /= len(test_loader.dataset)
     print('\nTest Set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, accuracy_counter, len(test_loader.dataset), 100. * accuracy_counter / len(test_loader.dataset)))
-
+    """
     return prediction_list
 
 
+# predict test_y on the test_x file
+def predict_y(model, test_x):
+    model.eval()
+    test_y_list = []
+    for data in test_x:
+        output = model(data)
+        predict = output.max(1, keepdim=True)[1]
+        test_y_list.append(str(int(predict)))
+    return test_y_list
+
+
+# write the predictions y to file
 def write_to_file(predict_y):
     with open('test_y', 'w') as file:
         for y in predict_y:
@@ -200,7 +221,6 @@ class FirstNetwork(torch.nn.Module):
         x = x.view(-1, self.image_size)
         x = nn.relu(self.fc0(x))
         x = nn.relu(self.fc1(x))
-        # x = nn.relu(self.fc2(x))
         x = self.fc2(x)
         return nn.log_softmax(x, dim=1)
 
@@ -229,7 +249,6 @@ class FirstNetworkWithDropout(torch.nn.Module):
         m = torch.nn.Dropout(p=0.5)
         x = m(x)
 
-        # x = nn.relu(self.fc2(x))
         x = self.fc2(x)
         return nn.log_softmax(x, dim=1)
 
@@ -256,7 +275,6 @@ class FirstNetworkWithNormalization(torch.nn.Module):
         return nn.log_softmax(x, dim=1)
 
 
-# todo !!!! notice that on model E, F we don't know which optimizer works best. So, we need to check all possibilities.
 # Five hidden layers: Model E
 class SecondNetwork(torch.nn.Module):
     def __init__(self, image_size):
@@ -277,7 +295,6 @@ class SecondNetwork(torch.nn.Module):
         x = x.view(-1, self.image_size)
         x = nn.relu(self.fc0(x))
         x = nn.relu(self.fc1(x))
-        # x = nn.relu(self.fc2(x))
         x = self.fc2(x)
         return nn.log_softmax(x, dim=1)
 
@@ -302,7 +319,6 @@ class ThirdNetwork(torch.nn.Module):
         x = x.view(-1, self.image_size)
         x = torch.sigmoid(self.fc0(x))
         x = torch.sigmoid(self.fc1(x))
-        # x = torch.sigmoid(self.fc2(x))
         x = self.fc2(x)
         return nn.log_softmax(x, dim=1)
 
